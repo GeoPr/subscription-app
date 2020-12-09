670aa6b4a068946428377e50f4f3a2dd5e0766fb
George <klassnoyparen@gmail.com>
2020-12-09 19:03:05 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './index.scss'
 import { render } from 'react-dom'
-import App from './assets/components/App'
+import App from './components/App'
 import { Provider } from 'react-redux'
-import { store } from './assets/redux/store'
-import { ContextProvider } from './assets/context/ContextProvider'
+import { store } from './redux/store'
+import { ContextProvider } from './context/ContextProvider'
 
 render(
   <Provider store={store}>
